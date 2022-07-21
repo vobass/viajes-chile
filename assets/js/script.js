a24f3dd7295d28a1391ad4e5c625d66ec17c2ea6
vobass <bassm.thiele@gmail.com>
2022-07-21 16:42:25 -0400

    proyecto completo

---
+++ assets/js/script.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+    // Smooth scroll
+    $("a").on('click', function (event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+            var hash = this.hash;
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function () {
+                window.location.hash = hash;
+            });
+        }
+    });
+
+    //Intervalo de imagenes carrusel
+    $('#carousel-inicial').carousel({
+        interval: 5000
+
+    });
+});
